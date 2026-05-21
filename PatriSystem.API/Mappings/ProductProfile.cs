1f60bfb6f12c3af258cd1d837f2b0c6fbd13aeb2
sebastiancarrojas <sebastiancarrojas@gmail.com>
2026-05-20 21:07:08 -0500

    feat: add get, update and deactivate product endpoints

--- PatriSystem.API/Mappings/ProductProfile.cs
+++ PatriSystem.API/Mappings/ProductProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using PatriSystem.API.DTOs.Request;
+using PatriSystem.API.DTOs.Response;
 using PatriSystem.Domain.Entities;
 
 namespace PatriSystem.API.Mappings
@@ -9,6 +10,10 @@ namespace PatriSystem.API.Mappings
         public ProductProfile()
         {
             CreateMap<CreateProductRequestDto, Product>();
+            CreateMap<UpdateProductRequestDto, Product>();
+            CreateMap<Product, ProductResponseDto>()
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.CategoryName))
+                .ForMember(dest => dest.BrandName, opt => opt.MapFrom(src => src.Brand.BrandName));
         }
     }
 }

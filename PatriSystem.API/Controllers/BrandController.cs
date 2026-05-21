e39904b2280f1e3d8856bf8fcc52e865e9c0292d
sebastiancarrojas <sebastiancarrojas@gmail.com>
2026-05-20 21:50:19 -0500

    feat: add categories and brands endpoints

---
+++ PatriSystem.API/Controllers/BrandController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PatriSystem.API.DTOs.Request;
+using PatriSystem.API.DTOs.Response;
+using PatriSystem.Domain.Entities;
+using PatriSystem.Domain.Interfaces.Services;
+
+namespace PatriSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandService _brandService;
+        private readonly IMapper _mapper;
+
+        public BrandsController(IBrandService brandService, IMapper mapper)
+        {
+            _brandService = brandService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _brandService.GetAllAsync();
+            if (!response.IsSuccess)
+                return BadRequest(response);
+
+            var dto = _mapper.Map<List<BrandResponseDto>>(response.Result);
+            return Ok(dto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateBrandRequestDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var brand = _mapper.Map<Brand>(dto);
+            var response = await _brandService.CreateAsync(brand);
+
+            if (!response.IsSuccess)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ PatriSystem.Domain/Interfaces/Repositories/IBrandRepository.cs
@@ -0,0 +1,11 @@
+﻿using PatriSystem.Domain.Entities;
+
+namespace PatriSystem.Domain.Interfaces.Repositories
+{
+    public interface IBrandRepository
+    {
+        Task<List<Brand>> GetAllAsync();
+        Task CreateAsync(Brand brand);
+        Task<bool> ExistsWithNameAsync(string name);
+    }
+}

4240d06d425c68019615dfa05c0a3d59d92ac661
sebastiancarrojas <sebastiancarrojas@gmail.com>
2026-05-20 22:26:10 -0500

    feat: add sales module with create and list endpoints

---
+++ PatriSystem.API/DTOs/Response/SaleResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PatriSystem.API.DTOs.Response
+{
+    public class SaleResponseDto
+    {
+        public Guid Id { get; set; }
+        public DateTime SaleDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public List<SaleDetailResponseDto> SaleDetails { get; set; } = new();
+    }
+}

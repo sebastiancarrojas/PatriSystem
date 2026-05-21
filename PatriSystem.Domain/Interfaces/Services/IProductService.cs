d89c224e2319a9dd1dd9189c4740892a5b516eeb
sebastiancarrojas <sebastiancarrojas@gmail.com>
2026-05-20 21:07:08 -0500

    feat: add get, update and deactivate product endpoints

--- PatriSystem.Domain/Interfaces/Services/IProductService.cs
+++ PatriSystem.Domain/Interfaces/Services/IProductService.cs
@@ -6,5 +6,9 @@ namespace PatriSystem.Domain.Interfaces.Services
     public interface IProductService
     {
         Task<Response<object>> CreateAsync(Product product);
+        Task<Response<object>> UpdateAsync(Guid id, Product product);
+        Task<Response<object>> DeactivateAsync(Guid id);
+        Task<Response<List<Product>>> GetAllAsync();
+        Task<Response<Product>> GetByIdAsync(Guid id);
     }
 }

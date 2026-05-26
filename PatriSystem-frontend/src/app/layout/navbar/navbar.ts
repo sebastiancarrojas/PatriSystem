1b579e2373358782a4f211cb624476ae7661aa70
sebastiancarrojas <sebastiancarrojas@gmail.com>
2026-05-26 18:59:44 -0500

    feat: add navbar layout component

---
+++ PatriSystem-frontend/src/app/layout/navbar/navbar.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class NavbarComponent {}

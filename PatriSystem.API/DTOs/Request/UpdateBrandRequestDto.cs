328b0733627283e6f15220440fbfddb758974d14
SebasCR <sebastiancarrojas@gmail.com>
2026-05-30 21:40:12 -0500

    feat: add pagination and update endpoints for categories and brands

---
+++ PatriSystem.API/DTOs/Request/UpdateBrandRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatriSystem.API.DTOs.Request
+{
+    public class UpdateBrandRequestDto
+    {
+        [Required(ErrorMessage = "El nombre de la marca es requerido")]
+        public string BrandName { get; set; } = string.Empty;
+        public string BrandDescription { get; set; } = string.Empty;
+    }
+}

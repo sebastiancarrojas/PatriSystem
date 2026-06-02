3cadc41e78dc3b98c2f92f40ecebbf77b1bedd2d
SebasCR <sebastiancarrojas@gmail.com>
2026-06-02 00:59:18 -0500

    feat: add Dashboard module (repository, service, controller)

---
+++ PatriSystem.API/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatriSystem.Domain.Interfaces.Services;
+
+namespace PatriSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IDashboardService _dashboardService;
+
+        public DashboardController(IDashboardService dashboardService)
+        {
+            _dashboardService = dashboardService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var response = await _dashboardService.GetDashboardAsync();
+            if (!response.IsSuccess)
+                return BadRequest(response);
+            return Ok(response.Result);
+        }
+    }
+}
---
+++ PatriSystem.Domain/Services/DashboardService.cs
@@ -0,0 +1,46 @@
+﻿using PatriSystem.Domain.Common;
+using PatriSystem.Domain.Interfaces.Repositories;
+using PatriSystem.Domain.Interfaces.Services;
+
+namespace PatriSystem.Domain.Services
+{
+    public class DashboardService : IDashboardService
+    {
+        private readonly IDashboardRepository _dashboardRepository;
+
+        public DashboardService(IDashboardRepository dashboardRepository)
+        {
+            _dashboardRepository = dashboardRepository;
+        }
+
+        public async Task<Response<object>> GetDashboardAsync()
+        {
+            try
+            {
+                var salesToday = await _dashboardRepository.GetSalesTodayAsync();
+                var totalToday = await _dashboardRepository.GetTotalTodayAsync();
+                var totalProducts = await _dashboardRepository.GetTotalProductsAsync();
+                var activeProducts = await _dashboardRepository.GetActiveProductsAsync();
+                var totalSales = await _dashboardRepository.GetTotalSalesAsync();
+                var totalRevenue = await _dashboardRepository.GetTotalRevenueAsync();
+
+                var dashboard = new
+                {
+                    salesToday,
+                    totalToday,
+                    totalProducts,
+                    activeProducts,
+                    inactiveProducts = totalProducts - activeProducts,
+                    totalSales,
+                    totalRevenue
+                };
+
+                return Response<object>.Success(dashboard);
+            }
+            catch (Exception ex)
+            {
+                return Response<object>.Failure(ex, "Error al obtener el dashboard");
+            }
+        }
+    }
+}

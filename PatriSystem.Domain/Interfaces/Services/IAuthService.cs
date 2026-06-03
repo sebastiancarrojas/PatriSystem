de9846d60e935685c0d9eb16174acd6e59ed4d70
SebasCR <sebastiancarrojas@gmail.com>
2026-06-02 20:39:32 -0500

    feat: add JWT authentication with ASP.NET Core Identity

---
+++ PatriSystem.Domain/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using PatriSystem.Domain.Common;
+
+namespace PatriSystem.Domain.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<Response<string>> LoginAsync(string email, string password);
+    }
+}

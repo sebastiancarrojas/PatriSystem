2a1184d18dba3e80cf17cf6bf21b48a87bb2166e
sebastiancarrojas <sebastiancarrojas@gmail.com>
2026-06-03 22:33:13 -0500

    fix: timezone chart dates and add realtime clock to dashboard

--- PatriSystem-frontend/src/app/app.config.ts
+++ PatriSystem-frontend/src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { registerLocaleData } from '@angular/common';
+import localeEsCO from '@angular/common/locales/es-CO';
+
+registerLocaleData(localeEsCO);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'es-CO' }
   ]
 };
